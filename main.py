0dc18fd5ee84a41da919550e2398a815f63ecfb9
jordan <jordanmaulana26@gmail.com>
2025-03-09 22:29:36 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# streamlit run main.py
+
+import streamlit as st
+
+from models import Me, Person, Transaction
+
+st.title("Gue hutang siapa ???!")
+
+if "me" not in st.session_state:
+    st.session_state.me = Me()
+
+page = st.sidebar.selectbox(
+    "Menu",
+    ["Hutang Gue", "Temen Gue"],
+)
+
+
+if page == "Temen Gue":
+    st.write("### Tambah Temen")
+    name = st.text_input("Nama")
+    phone_number = st.text_input("Nomor HP", help="Format: 08xx-xxxx-xxxx")
+
+    button = st.button("Tambah Temen")
+
+    if button:
+        new_member = Person(name, phone_number)
+        st.session_state.me.add_friend(new_member)
+        st.rerun()
+
+    st.write("### Teman Gue")
+    if not st.session_state.me.friends:
+        st.write("Belum punya temen, kasian gue :(")
+    else:
+        for friend in st.session_state.me.friends:
+            st.write(friend.name)
+            amount = int(
+                st.number_input(
+                    "Duit",
+                    key=f"{friend.name}_amount",
+                    value=0,
+                    step=1000,
+                )
+            )
+            lend = st.button("Aku minjemin", key=f"{friend.name}_lend")
+            borrow = st.button("Aku hutang", key=f"{friend.name}_borrow")
+
+            if lend:
+                transaction = Transaction(amount)
+                friend.transactions.append(transaction)
+                st.rerun()
+
+            if borrow:
+                transaction = Transaction(amount)
+                transaction.ask()
+                friend.transactions.append(transaction)
+                st.rerun()
+
+
+elif page == "Hutang Gue":
+    if not st.session_state.me.friends:
+        st.write("Belum punya temen, kasian gue :(")
+
+    else:
+        friends = st.session_state.me.friends_has_debt()
+        if not friends:
+            st.write("Semua teman gue baik-baik aja, ga ada yang hutang sama gue :)")
+        else:
+            for friend in friends:
+                st.write(friend.__str__())
---
+++ models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+class Person:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        self.transactions = []
+
+    def __str__(self):
+        total = sum([transaction.amount for transaction in self.transactions])
+        if total > 0:
+            return f'{self.name} masih hutang ke aku {total}'
+        return f'Aku masih hutang {total*-1} ke {self.name}'
+    
+class Transaction:
+
+    """
+        1. I lend money to my friend -> +amount on that person
+        2. I ask money to my friend -> -amount on that person
+    """
+
+    # As lend function (1)
+    def __init__(self, amount: int):
+        self.amount = amount
+        self.created_at = datetime.now()
+
+    # As ask function (2)
+    def ask(self):
+        self.amount *= -1
+
+
+class Me:
+    def __init__(self):
+        self.friends = []
+
+    def add_friend(self, friend: Person):
+        self.friends.append(friend)
+
+    def friends_has_debt(self):
+        return [friend for friend in self.friends if friend.transactions]
